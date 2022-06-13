4705ba1debd85a6f58a281b19a6600c8b37508a2
VincentD4b <vincent.vacher@digital4better.com>
2022-06-13 17:50:02 +0200

    refacto of vision widget for better lifecycle management

--- lib/widgets/vision.dart
+++ lib/widgets/vision.dart
@@ -23,19 +23,17 @@ class QuadPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawRect(
-        Offset(0, 0) & size, Paint()..color = Color.fromARGB(alpha, 0, 0, 0));
     if (this.quad != null && this.draw) {
       late Path path;
       if (Platform.isAndroid) {
         path = Path()
-          ..moveTo((1-this.quad!.topRight.y) * size.width,
+          ..moveTo((1 - this.quad!.topRight.y) * size.width,
               this.quad!.topRight.x * size.height)
-          ..lineTo((1-this.quad!.topLeft.y) * size.width,
+          ..lineTo((1 - this.quad!.topLeft.y) * size.width,
               this.quad!.topLeft.x * size.height)
-          ..lineTo((1-this.quad!.bottomLeft.y) * size.width,
+          ..lineTo((1 - this.quad!.bottomLeft.y) * size.width,
               this.quad!.bottomLeft.x * size.height)
-          ..lineTo((1-this.quad!.bottomRight.y) * size.width,
+          ..lineTo((1 - this.quad!.bottomRight.y) * size.width,
               this.quad!.bottomRight.x * size.height)
           ..close();
       } else {
@@ -56,8 +54,7 @@ class QuadPainter extends CustomPainter {
           Paint()
             ..color = Color.fromARGB(alpha, 255, 255, 255)
             ..style = PaintingStyle.stroke
-            ..strokeWidth = 10
-      );
+            ..strokeWidth = 10);
     }
   }
 
@@ -72,70 +69,151 @@ class Vision extends StatefulWidget {
   VisionState createState() => VisionState();
 }
 
-void _detectQuad(data) {
-  SendPort? sendPort;
-  final receivePort = ReceivePort();
-  receivePort.listen((data) async {
+void _detectQuad(SendPort sendPort) async {
+  ReceivePort spawnReceivePort = ReceivePort();
+  sendPort.send(spawnReceivePort.sendPort);
+
+  await for (dynamic msg in spawnReceivePort) {
+    var data = msg[0];
+    SendPort replyTo = msg[1];
+
     if (data is CameraImage) {
       try {
-        sendPort?.send(Quad.from(detectQuad(data)));
+        replyTo.send(Quad.from(detectQuad(data)));
       } catch (_) {
-        sendPort?.send(Quad.empty);
+        replyTo.send(Quad.empty);
       }
     }
-  });
-  if (data is SendPort) {
-    sendPort = data;
-    sendPort.send(receivePort.sendPort);
-    return;
+
+    if (data is String && data == "close") {
+      spawnReceivePort.close();
+    }
   }
+}
+
+/// sends a message on a port, receives the response,
+/// and returns the message
+Future sendReceive(SendPort port, msg) {
+  ReceivePort response = new ReceivePort();
+  port.send([msg, response.sendPort]);
+  return response.first;
 }
 
 class VisionState extends State<Vision>
     with WidgetsBindingObserver, SingleTickerProviderStateMixin {
-  late CameraDescription _camera;
-  late CameraController _controller;
+  final int minTickSpeed = 60;
+  final int maxTickSpeed = 3;
+  late String _imagesRootPath;
+  late ReceivePort _receivePort;
+  late SendPort _sendPort;
   late Ticker _ticker;
-  late Size _size;
-  late double _deviceRatio;
-  bool _isScanning = false;
-  bool _isDetecting = false;
+  int _tick = 0;
+  //int? _lastTickTimestamp;
+  CameraController? _cameraController;
+  CameraDescription? _camera;
+  CameraImage? _lastCameraImage;
+  bool _isDetectingQuad = false;
+  Quad _currentQuad = Quad.empty;
+  Quad _previousQuad = Quad.empty;
   Timer? _hapticTimer;
 
-  late String _imagesRootPath;
+  // QUAD DETECTION FUNCTIONS //
+  Future _startQuadDetection() async {
+    _tick = 0;
+    _ticker.start();
 
-  final _receivePort = ReceivePort();
-  SendPort? _isolatePort;
+    await _cameraController!.startImageStream((CameraImage image) async {
+      if (!_isDetectingQuad) {
+        // Delegate detecting quad heavy task to an isolate thread.
+        _isDetectingQuad = true;
+        _currentQuad = await sendReceive(_sendPort, image);
 
-  Quad target = Quad.empty;
-  CameraImage? _lastCameraImage;
+        // Once it's done, check if widget hasn't been close in the meanwhile.
+        if (_cameraController == null ||
+            !_cameraController!.value.isInitialized ||
+            !_cameraController!.value.isStreamingImages) {
+          return;
+        }
 
-  Quad current = Quad.empty;
-  int alpha = 0;
-  int time = 0;
+        // If everything is fine, then update detected quad state.
+        _isDetectingQuad = false;
+        _lastCameraImage = image;
+      }
+    });
+    await _cameraController!.setFlashMode(FlashMode.torch);
 
-  Future _initImagesPath() async {
-    _imagesRootPath = (await getTemporaryDirectory()).path;
+    Speech().speak(
+        "Scan de document prêt, présentez un document devant l’appareil.");
   }
 
-  /// Makes sure that camera stream and torch is off if app is not running.
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    if (_controller.value.isInitialized) {
-      if (state == AppLifecycleState.resumed && _isScanning) {
-        await _startImageStream();
-      } else {
-        await _stopImageStream();
-        await _controller.setFlashMode(FlashMode.off);
+  Future _stopQuadDetection({bool isKeepFlashOn = false}) async {
+    //alpha = 0;
+    _ticker.stop();
+
+    if (_cameraController != null &&
+        _cameraController!.value.isStreamingImages) {
+      await _cameraController!.stopImageStream();
+      _isDetectingQuad = false;
+
+      if (!isKeepFlashOn) {
+        await _cameraController!.setFlashMode(FlashMode.off);
       }
+
+      sendReceive(_sendPort, "close");
     }
   }
 
-  /// Defines duration between two vibration.
-  /// The largest the detected sheet is, the less time there is between two vibrations.
-  Duration? timeBetweenTwoVibrations() {
-    double widthPercent = min(current.topRight.x - current.topLeft.x,
-            current.bottomRight.x - current.bottomLeft.x)
+  _tickEmptyQuad() {
+    _stopHapticFeedback();
+
+    setState(() {
+      //alpha = max(alpha - alphaSpeed, 0);
+      //if (alpha == 0) {
+      //  _previousDetectedQuad = Quad.empty;
+      //}
+    });
+  }
+
+  _tickDetectedQuad() {
+    _startHapticFeedback();
+
+    setState(() {
+      //alpha = min(alpha + alphaSpeed, 255);
+      if (_previousQuad.isEmpty) {
+        _previousQuad = _currentQuad;
+      } else {
+        _previousQuad.topLeft +=
+            (_currentQuad.topLeft * 0.1 - _previousQuad.topLeft * 0.1);
+        _previousQuad.topRight +=
+            (_currentQuad.topRight * 0.1 - _previousQuad.topRight * 0.1);
+        _previousQuad.bottomRight +=
+            (_currentQuad.bottomRight * 0.1 - _previousQuad.bottomRight * 0.1);
+        _previousQuad.bottomLeft +=
+            (_currentQuad.bottomLeft * 0.1 - _previousQuad.bottomLeft * 0.1);
+      }
+    });
+
+    // TODO add vocal instructions
+    // TODO add movement detection for better capture
+    //if (alpha == 255) {
+    num widthPercent = min(_previousQuad.topRight.x - _previousQuad.topLeft.x,
+        _previousQuad.bottomRight.x - _previousQuad.bottomLeft.x);
+
+    if (widthPercent > 0.75) {
+      //takePictureForAnalyse();
+    } else if (_tick >
+        (maxTickSpeed - minTickSpeed) * ((0.6 - min(0.6, widthPercent)) / 0.6) +
+            maxTickSpeed) {
+      _tick = 0;
+    }
+    //}
+    //}
+  }
+
+  // HAPTICS FEEDBACK FUNCTIONS //
+  Duration? _timeBetweenTwoVibrations() {
+    double widthPercent = min(_currentQuad.topRight.x - _currentQuad.topLeft.x,
+            _currentQuad.bottomRight.x - _currentQuad.bottomLeft.x)
         .toDouble();
 
     if (widthPercent <= 0) {
@@ -154,9 +232,9 @@ class VisionState extends State<Vision>
 
   /// When user is close to frame correctly the document, haptics feedbacks starts.
   /// The more it vibrates, the better the frame is.
-  Future startHapticFeedback() async {
+  Future _startHapticFeedback() async {
     if (_hapticTimer == null || !_hapticTimer!.isActive) {
-      Duration? duration = timeBetweenTwoVibrations();
+      Duration? duration = _timeBetweenTwoVibrations();
       if (duration.runtimeType == Duration) {
         await HapticFeedback.selectionClick();
         _hapticTimer = Timer(duration!, () {});
@@ -165,160 +243,27 @@ class VisionState extends State<Vision>
   }
 
   /// Stops haptics feedbacks.
-  void stopHapticFeedback() {
+  void _stopHapticFeedback() {
     if (_hapticTimer != null && _hapticTimer!.isActive) {
       _hapticTimer!.cancel();
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    _initialisation();
-
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  Future<void> _initialisation() async {
-    await _initCamera();
-    await _initDetection();
-    await _initAnimation();
-  }
-
-  Future<void> _initAnimation() async {
-    int tock = 0;
-    _ticker = createTicker((Duration elapsed) {
-      final int delta = elapsed.inMilliseconds - time;
-      final int alphaSpeed = (255 * delta / 300).round();
-      final int minTockSpeed = 60;
-      final int maxTockSpeed = 3;
-
-      if (target.isEmpty) {
-        stopHapticFeedback();
-
-        setState(() {
-          alpha = max(alpha - alphaSpeed, 0);
-          if (alpha == 0) {
-            current = Quad.empty;
-          }
-        });
-      } else {
-        startHapticFeedback();
-
-        setState(() {
-          alpha = min(alpha + alphaSpeed, 255);
-          if (current.isEmpty) {
-            current = target;
-          } else {
-            current.topLeft += (target.topLeft * 0.1 - current.topLeft * 0.1);
-            current.topRight +=
-                (target.topRight * 0.1 - current.topRight * 0.1);
-            current.bottomRight +=
-                (target.bottomRight * 0.1 - current.bottomRight * 0.1);
-            current.bottomLeft +=
-                (target.bottomLeft * 0.1 - current.bottomLeft * 0.1);
-          }
-        });
-        if (_isScanning) {
-          // TODO add vocal instructions
-          // TODO add movement detection for better capture
-          if (alpha == 255) {
-            num widthPercent = min(current.topRight.x - current.topLeft.x,
-                current.bottomRight.x - current.bottomLeft.x);
-            if (widthPercent > 0.75) {
-              takePictureForAnalyse();
-            } else if (tock >
-                (maxTockSpeed - minTockSpeed) *
-                        ((0.6 - min(0.6, widthPercent)) / 0.6) +
-                    maxTockSpeed) {
-              tock = 0;
-            }
-          }
-        }
-      }
-      time = elapsed.inMilliseconds;
-      tock++;
-    });
-    await _ticker.start();
-  }
-
-  Future<void> _initCamera() async {
-    await _initImagesPath();
-    final cameras = await availableCameras();
-
-    if (cameras.length > 0) {
-      _camera = cameras.firstWhere(
-        (element) => element.lensDirection == CameraLensDirection.back,
-        orElse: () => cameras.first,
-      );
-      _controller = CameraController(
-        _camera,
-        ResolutionPreset.ultraHigh,
-        enableAudio: false,
-        imageFormatGroup: Platform.isAndroid
-            ? ImageFormatGroup.yuv420
-            : ImageFormatGroup.bgra8888,
-      );
-      await _controller.initialize();
-      await _controller.lockCaptureOrientation(DeviceOrientation.portraitUp);
-      await _startImageStream();
-
-      setState(() {
-        _isScanning = true;
-      });
-    }
-  }
-
-  Future _startImageStream() async {
-    await _controller.startImageStream((CameraImage image) async {
-      if (_isDetecting || _isolatePort == null) return;
-      _isDetecting = true;
-      _isolatePort?.send(image);
-      _lastCameraImage = image;
-    });
-    await _controller.setFlashMode(FlashMode.torch);
-    Speech().speak(
-        "Scan de document prêt, présentez un document devant l’appareil.");
-  }
-
-  Future _stopImageStream() async {
-    alpha = 0;
-
-    if (_controller.value.isStreamingImages) {
-      await _controller.stopImageStream();
-    }
-    _isDetecting = false;
-  }
-
-  Future<void> _initDetection() async {
-    await Isolate.spawn<SendPort>(
-      _detectQuad,
-      _receivePort.sendPort,
-      onError: _receivePort.sendPort,
-      onExit: _receivePort.sendPort,
-    );
-
-    _receivePort.listen((data) {
-      if (data is SendPort) {
-        _isolatePort = data;
-        _isolatePort = data;
-      } else if (data is Quad) {
-        setState(() {
-          _isDetecting = false;
-          target = data;
-        });
-      }
+  void _resetQuads() {
+    setState(() {
+      _currentQuad = Quad.empty;
+      _previousQuad = Quad.empty;
+      //alpha = 0;
     });
   }
 
-  Future<void> takePictureForAnalyseForIos() async {
-    Speech()
+  // DO ANALYSE FUNCTIONS //
+  Future takePictureForAnalyseForIos() async {
+    await Speech()
         .speak("Capture effectuée. Traitement en cours, veuillez patienter.");
 
     // Stop preview stream and take a picture from camera.
-    await _stopImageStream();
-    await _controller.setFlashMode(FlashMode.off);
+    await _stopQuadDetection();
 
     BGRImage picture = cameraImageToBGRBytes(_lastCameraImage!);
 
@@ -326,13 +271,9 @@ class VisionState extends State<Vision>
     String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
     String warpedFileName = "${timestamp}-warpedPicture.png";
     String warpedPath = _imagesRootPath + "/$warpedFileName";
-    warpImage(picture, current, warpedPath);
+    warpImage(picture, _previousQuad, warpedPath);
 
-    setState(() {
-      current = Quad.empty;
-      target = Quad.empty;
-      alpha = 0;
-    });
+    _resetQuads();
 
     await Navigator.pushReplacement(
       context,
@@ -344,13 +285,13 @@ class VisionState extends State<Vision>
     );
   }
 
-  Future<void> takePictureForAnalyseForAndroid() async {
+  Future takePictureForAnalyseForAndroid() async {
     Speech().speak("Capture en cours, ne bougez plus votre appareil.");
 
     // Stop preview stream and take a picture from camera.
-    await _stopImageStream();
-    XFile picture = await _controller.takePicture();
-    await _controller.setFlashMode(FlashMode.off);
+    await _stopQuadDetection(isKeepFlashOn: true);
+    XFile picture = await _cameraController!.takePicture();
+    await _cameraController!.setFlashMode(FlashMode.off);
 
     // Save raw picture file somewhere on the phone.
     String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
@@ -362,6 +303,8 @@ class VisionState extends State<Vision>
     String warpedPath = _imagesRootPath + "/$warpedFileName";
     await picture.saveTo(warpedPath);
 
+    _resetQuads();
+
     await Navigator.pushReplacement(
       context,
       MaterialPageRoute(
@@ -372,14 +315,11 @@ class VisionState extends State<Vision>
     );
   }
 
-  Future<void> takePictureForAnalyse() async {
+  Future takePictureForAnalyse() async {
     if (_lastCameraImage != null &&
-        _isScanning &&
-        _controller.value.isInitialized) {
-      setState(() {
-        _isScanning = false;
-      });
-      stopHapticFeedback();
+        _cameraController != null &&
+        _cameraController!.value.isInitialized) {
+      _stopHapticFeedback(); // TODO check if useful
 
       if (Platform.isAndroid) {
         takePictureForAnalyseForAndroid();
@@ -391,6 +331,7 @@ class VisionState extends State<Vision>
 
   void goToMainMenu() async {
     Speech().speak("Retour au menu principal");
+
     await Navigator.pushReplacement(
       context,
       MaterialPageRoute(
@@ -401,20 +342,128 @@ class VisionState extends State<Vision>
     );
   }
 
+  // LIFECYCLE WIDGET FUNCTIONS //
   @override
-  void dispose() {
-    _ticker.dispose();
-    _controller.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
+  void initState() {
+    _init();
+
+    WidgetsBinding.instance.addObserver(this);
+
+    super.initState();
+  }
+
+  Future _init() async {
+    await _initIsolatePort();
+    await _initCamera();
+    await _initTicker();
+
+    await _startQuadDetection();
+  }
+
+  Future _initImagesPath() async {
+    _imagesRootPath = (await getTemporaryDirectory()).path;
+  }
+
+  Future _initCamera() async {
+    await _initImagesPath();
+
+    try {
+      List<CameraDescription> cameras = await availableCameras();
+
+      if (cameras.length > 0) {
+        _camera = cameras.firstWhere(
+          (element) => element.lensDirection == CameraLensDirection.back,
+          orElse: () => cameras.first,
+        );
+
+        _cameraController = CameraController(
+          _camera!,
+          ResolutionPreset.ultraHigh,
+          enableAudio: false,
+          imageFormatGroup: Platform.isAndroid
+              ? ImageFormatGroup.yuv420
+              : ImageFormatGroup.bgra8888,
+        );
+        await _cameraController!.initialize();
+        await _cameraController
+            ?.lockCaptureOrientation(DeviceOrientation.portraitUp);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  /// Detecting quad is an heavy task, that's why it will be delegate in an isolate port.
+  /// In order to avoid UI freeze.
+  Future _initIsolatePort() async {
+    _receivePort = ReceivePort();
+
+    await Isolate.spawn<SendPort>(
+      _detectQuad,
+      _receivePort.sendPort,
+    );
+
+    _sendPort = await _receivePort.first;
+  }
+
+  Future _initTicker() async {
+    //_lastTickTimestamp = 0;
+    _ticker = createTicker((Duration elapsed) {
+      //final int delta = elapsed.inMilliseconds - _lastTickTimestamp!;
+      //final int alphaSpeed = (255 * delta / 300).round();
+
+      if (_currentQuad.isEmpty) {
+        _tickEmptyQuad();
+      } else {
+        _tickDetectedQuad();
+      }
+
+      //_lastTickTimestamp = elapsed.inMilliseconds;
+      _tick++;
+    });
+  }
+
+  @override
+  void dispose() async {
+    try {
+      if (_cameraController != null) {
+        _cameraController!.dispose();
+      }
+
+      _ticker.dispose();
+
+      if (_hapticTimer != null && _hapticTimer!.isActive) {
+        _hapticTimer!.cancel();
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      WidgetsBinding.instance.removeObserver(this);
+      super.dispose();
+    }
+  }
+
+  /// Makes sure that camera stream and torch is off if app is not running.
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (_cameraController != null && _cameraController!.value.isInitialized) {
+      if (state == AppLifecycleState.resumed &&
+          !_cameraController!.value.isStreamingImages) {
+        await _startQuadDetection();
+      } else {
+        await _stopQuadDetection();
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    _size = MediaQuery.of(context).size;
-    _deviceRatio = _size.width / _size.height;
-    final ratio =
-        _isScanning ? _controller.value.aspectRatio * _deviceRatio : 1.0;
+    Size size = MediaQuery.of(context).size;
+    double deviceRatio = size.width / size.height;
+    double ratio =
+        _cameraController != null && _cameraController!.value.isInitialized
+            ? _cameraController!.value.aspectRatio * deviceRatio
+            : 1.0;
 
     final scale = 1 / ratio;
 
@@ -423,15 +472,16 @@ class VisionState extends State<Vision>
         scale: scale,
         child: CustomPaint(
           foregroundPainter: QuadPainter(
-            quad: current,
-            draw: _isScanning,
-            alpha: 100,
+            quad: _previousQuad,
+            draw: !_currentQuad.isEmpty,
+            alpha: 255,
           ),
           child: GestureDetector(
             onLongPress: takePictureForAnalyse,
             onDoubleTap: goToMainMenu,
-            child: _isScanning && _controller.value.isInitialized
-                ? CameraPreview(_controller)
+            child: _cameraController != null &&
+                    _cameraController!.value.isInitialized
+                ? CameraPreview(_cameraController!)
                 : Container(),
           ),
         ),
