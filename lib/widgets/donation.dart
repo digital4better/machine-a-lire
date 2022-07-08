bd7e1361b98c6cb453d64a2b5a0e6acc0c04434c
VincentD4b <vincent.vacher@digital4better.com>
2022-07-08 14:15:58 +0200

    fix issue with big image in donation screen when TalkBack is active, focus is lost.

--- lib/widgets/donation.dart
+++ lib/widgets/donation.dart
@@ -34,40 +34,52 @@ class Donation extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "C’est grâce à vos dons que l’ACIAH peut oeuvrer pour un numérique accessible à tous.",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                          fontWeight: FontWeight.w700,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: Text(
+                          "C’est grâce à vos dons que l’ACIAH peut oeuvrer pour un numérique accessible à tous.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
-                      const Padding(padding: EdgeInsets.only(bottom: 30)),
-                      const Image(
-                        image: AssetImage("assets/images/aciah.png"),
-                        width: 150,
-                      ),
-                      const Padding(padding: EdgeInsets.only(bottom: 50)),
-                      Text(
-                        "MALO est une initiative portée par l’association ACIAH (Accessibilité, Communication, Information, Accompagnement du Handicap).\n\nL’application est et restera gratuite pour rester accessible à tous.\n\nPour mener à bien tous ses projets, l’ACIAH a besoin de fonds. En soutenant l’ACIAH vous participez au changement.",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50),
+                        child: SizedBox(
+                          height: 100,
+                          child: Image(
+                            image: AssetImage("assets/images/aciah.png"),
+                            width: 70,
+                            fit: BoxFit.scaleDown,
+                          ),
                         ),
                       ),
-                      const Padding(padding: EdgeInsets.only(bottom: 30)),
-                      MaloButton(
-                        prefixIcon: Icons.accessibility_sharp,
-                        text: "Faire un don",
-                        onPress: () async {
-                          try {
-                            await launchUrl(Uri.parse(donationUrl));
-                          } catch (e) {
-                            throw 'Could not launch $donationUrl (reason: $e)';
-                          }
-                        },
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30),
+                        child: Text(
+                          "MALO est une initiative portée par l’association ACIAH (Accessibilité, Communication, Information, Accompagnement du Handicap).\n\nL’application est et restera gratuite pour rester accessible à tous.\n\nPour mener à bien tous ses projets, l’ACIAH a besoin de fonds. En soutenant l’ACIAH vous participez au changement.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
                       ),
-                      const Padding(padding: EdgeInsets.only(bottom: 50)),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50),
+                        child: MaloButton(
+                          prefixIcon: Icons.accessibility_sharp,
+                          text: "Faire un don",
+                          onPress: () async {
+                            try {
+                              await launchUrl(Uri.parse(donationUrl));
+                            } catch (e) {
+                              throw 'Could not launch $donationUrl (reason: $e)';
+                            }
+                          },
+                        ),
+                      ),
                     ],
                   ),
                 ),
